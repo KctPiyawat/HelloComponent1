58bfd7a15c02c53883aa39d0d7869e5113dce470
piyawat <kct.thai@gmail.com>
2018-06-04 11:42:19 +0700

    Create Green Component Finish

---
+++ src/app/redcomponent/redcomponent.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-redcomponent',
+  templateUrl: './redcomponent.component.html',
+  styleUrls: ['./redcomponent.component.css']
+})
+export class RedcomponentComponent implements OnInit {
+
+  nameRedComponent = 'Red Component';
+
+  onclickRedComponent() {
+    window.alert('You Click ' + this.nameRedComponent);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+} // Main Class
